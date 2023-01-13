64b8594c1e5576e7b24e61653917e156db178539
Костя <97756865+conny-coder@users.noreply.github.com>
2023-01-13 19:08:10 +0200

    Update send-request.ts

--- pages/api/send-request.ts
+++ pages/api/send-request.ts
@@ -1,12 +1,6 @@
 import sendEmail from '../../app/mail'
 
 export default async function handler(req: any, res: any) {
-  const message = {
-    to: 'kostia.shkambula@gmail.com',
-    from: 'user@gmail.com',
-    subject: `Повідомлення з сайту "PORTFOLIO" от ${req.body.name}`,
-    text: `Имя: ${req.body.name},\nE-mail: ${req.body.email},\nСообщение: ${req.body.message}`,
-  }
-  sendEmail(message)
+  postMail(req, res)
   res.send(200)
 }
