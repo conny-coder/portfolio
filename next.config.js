22ca385cf88451cbb5c51794945b4bb4cee3d6cf
conny-coder <kostia.shkambula@gmail.com>
2023-01-15 13:30:05 +0200

    fourth commit

--- next.config.js
+++ next.config.js
@@ -9,20 +9,12 @@ const nextConfig = {
           { key: 'Access-Control-Allow-Origin', value: '*' },
           {
             key: 'Access-Control-Allow-Methods',
-<<<<<<< HEAD
             value: 'GET,POST',
           },
           {
             key: 'Access-Control-Allow-Headers',
             value:
               'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-=======
-            value: 'GET',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
->>>>>>> 53cb53e1bda7724837a24921b86b8a9892578b9d
           },
         ],
       },
@@ -31,7 +23,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: { domains: ['localhost'] },
   env: {
-    APP_URL: process.env.REACT_APP_URL,
+    APP_URL: process.env.APP_URL,
     APP_ENV: process.env.REACT_APP_ENV,
   },
 }
